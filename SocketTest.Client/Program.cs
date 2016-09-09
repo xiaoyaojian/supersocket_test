54348f220aa7622ff3a363bfe7e4c07c72b3856f
JamesWhite <xiaoyaojian@126.com>
2016-09-09 14:32:57 +0800

    Change ip address

--- SocketTest.Client/Program.cs
+++ SocketTest.Client/Program.cs
@@ -13,7 +13,7 @@ namespace SocketTest.Client
         static void Main(string[] args)
         {
             Console.ReadKey();
-            IPAddress ip = IPAddress.Parse("192.168.96.199");
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
             IPEndPoint endpoint = new IPEndPoint(ip, 6001);
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             socket.Connect(endpoint);
